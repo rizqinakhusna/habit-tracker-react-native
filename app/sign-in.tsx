f6f4b02830f3195b6f136aca442c3d1622e6f842
shuvo-me <shuvo.js.dev@gmail.com>
2025-10-03 12:46:46 +0600

    design: signin screen ui designed

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { KeyboardAvoidingView, Platform, View } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, Text, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SigninScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="h-screen">
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
+        className="flex flex-col h-full"
       >
-        <View>
-          <TextInput
-            label={"Email"}
-            autoCapitalize="none"
-            keyboardType="email-address"
-            placeholder="Enter email.."
-            mode="outlined"
-          />
-          <TextInput
-            label={"Password"}
-            autoCapitalize="none"
-            placeholder="Enter email.."
-            mode="outlined"
-          />
-          <Button>Sign In</Button>
+        <View className="my-auto px-5">
+          <Text variant="headlineLarge" className=" mx-auto mb-10">
+            Sign In
+          </Text>
+          <View className=" gap-6">
+            <TextInput
+              label={"Email"}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              placeholder="Enter email.."
+              mode="outlined"
+            />
+            <TextInput
+              secureTextEntry
+              label={"Password"}
+              placeholder="Enter password.."
+              mode="outlined"
+            />
+            <Button mode="outlined">Sign In</Button>
+          </View>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
